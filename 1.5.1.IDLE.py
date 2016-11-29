0b29d9d384854637f5adbebf54dd18d3de9cecad
高垚鑫 Gao Yaoxin, aka Felix Yaoxin Gao <love@honey.ga>
2016-11-29 20:15:03 +0800

    Create 1.5.1.IDLE.py

---
+++ 1.5.1.IDLE.py
@@ -0,0 +1,11 @@
+>>> "i love u"
+'i love u'
+>>> 'i love u'
+'i love u'
+>>> "Don't you love me?"
+"Don't you love me?"
+>>> "Don\'t you love me?"
+"Don't you love me?"
+>>> 'Don\'t you love me?'
+"Don't you love me?"
+>>> #使用双重引号或使用转义符
